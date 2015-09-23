804bdfc4926eaf2eb16fdb8550d6ccbaaee5fcc8
joylibingyao <joylibingyao@gmail.com>
2015-09-23 15:39:44 -0400

    Initial Commit

---
+++ gotcha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gotcha
+//
+//  Created by Bingyao Li on 9/23/15.
+//  Copyright © 2015 Bingyao Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
