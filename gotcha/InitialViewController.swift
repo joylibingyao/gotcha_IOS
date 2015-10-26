cc6cf0dc8f7be9a138ce7c83edab887980923a12
joylibingyao <joylibingyao@gmail.com>
2015-10-26 02:20:54 -0400

    added login and register page need to figure out where to add schedule

---
+++ gotcha/InitialViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InitialViewController.swift
+//  gotcha
+//
+//  Created by Bingyao Li on 10/26/15.
+//  Copyright © 2015 Bingyao Li. All rights reserved.
+//
+
+import UIKit
+class InitialViewController:UIViewController {
+    
+    @IBOutlet var email: UITextField!
+    @IBOutlet var password: UIView!
+    
+    @IBAction func fotgotPressed(sender: AnyObject) {
+    }
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+        //sned to back to validate info
+    }
+
+    
+    
+    
+    
+}
---
+++ gotcha/SignUpViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SignUpViewController.swift
+//  gotcha
+//
+//  Created by Bingyao Li on 10/26/15.
+//  Copyright © 2015 Bingyao Li. All rights reserved.
+//
+
+import UIKit
+class SignUpViewController:UIViewController {
+    
+    @IBOutlet var name: UITextField!
+    @IBOutlet var email: UITextField!
+    
+    @IBOutlet var password: UITextField!
+    
+    @IBOutlet var confirmPassword: UITextField!
+    
+    @IBAction func registerPressed(sender: AnyObject) {
+        //send all info to backend
+    }
+    
+    
+    
+    
+}
